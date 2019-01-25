6703041d2058df3de6f05f491985bc71858d6d46
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-25 11:17:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var breakOut = (array, changeValue, stopValue) => {
-  
+  for(let i=0; i<array.length; i++) {
+    
+  }
+}
+
+
+function firstNotEqual(array, value) {
+  for (let i = 0; i < array.length; i++) {
+    debugger
+    if (array[i] !== value) {
+      return array[i]
+    }
+  }
 }
