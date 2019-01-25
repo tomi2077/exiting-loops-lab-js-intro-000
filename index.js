1968ced00cdb2dfe0b539b8d961d9261c17a85a3
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-25 11:34:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,17 @@ var keepGoing = (array, changeValue, skipValue) => {
 }
 
 var findBy = (array,findFn) => {
-  
+ for (let i = 0; i < array.length; i++) {
+   if(array[i] == findFn){
+     break
+   }
+   return array[i]
+  }
+
+function find(array, criteriaFunc) {
+  for (let i = 0; i < array.length; i++) {
+    if (criteriaFunc(array[i])) {
+      return array[i]
+    }
+  }
 }
