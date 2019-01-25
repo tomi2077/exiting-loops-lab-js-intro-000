214374c05f05af44210349cd0b2692e080bd7ee7
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-25 11:29:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ var breakOut = (array, changeValue, stopValue) => {
   return array
 }
 
-
+var keepGoing = (array, changeValue, skipValue) => {
+  for (let i =0; i < array.length; i++) {
+    if(array[i] !== skipvalue){
+      continue;
+    }
+    array[i] = changeValue
+  }
+  return array
+}
