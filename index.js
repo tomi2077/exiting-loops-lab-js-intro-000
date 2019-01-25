014fa5e258e0b9321f4c421121ee1f45d0997c52
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-25 11:25:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 var breakOut = (array, changeValue, stopValue) => {
   for(let i=0; i<array.length; i++) {
     if (array[i] === stopValue){
-      break
+      break;
     }
-    array[i] !== changeValue
-  }
-      return array[changeValue]
-      if (array[changeValue]){
-        break;
-      }
-    }
-  }
-}
-
-
-function breakOut(array, changeValue, stopValue) {
-  for (let i = 0, l = array.length; i < l; i++) {
-    if (array[i] === stopValue) {
-      break
-    }
-
     array[i] = changeValue
   }
-
   return array
 }
+
+
